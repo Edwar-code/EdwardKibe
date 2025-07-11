f47ac45e0e1ac764f861a4a1ec19f64048e56bfb
Edwar-code <mbesha37@gmail.com>
2025-07-11 13:47:21 +0300

    Update import path for BuyMeACoffee component and refactor Home component structure

--- src/app/[locale]/page.tsx
+++ src/app/[locale]/page.tsx
@@ -1,15 +1,16 @@
-import Hero from '../sections/Hero';
-import Projects from '../sections/Projects';
-import Banner from '../sections/Banner';
-import References from '../sections/References';
-import About from '../sections/About';
-import Contact from '../sections/Contact';
-import Footer from '../sections/Footer';
-import Navbar from '../sections/Navbar';
-import BuyMeACoffee from '../sections/BuyMeACoffee';
+import Hero from '../sections/Hero'
+import Projects from '../sections/Projects'
+import Banner from '../sections/Banner'
+import References from '../sections/References'
+import About from '../sections/About'
+import Contact from '../sections/Contact'
+import Footer from '../sections/Footer'
+import Navbar from '../sections/Navbar'
+// Previous import was from '@/components/BuyMeACoffee'
+// This is the new, correct import path:
+import BuyMeACoffee from '../sections/BuyMeACoffee' // <-- THE PATH IS UPDATED HERE
 
-// This is the original, working version of your Home page.
-export default function Home() {
+function Home() {
   return (
     <div>
       <Navbar />
@@ -20,7 +21,9 @@ export default function Home() {
       <About />
       <Contact />
       <Footer />
-      <BuyMeACoffee />
+      <BuyMeACoffee /> {/* This part stays the same */}
     </div>
-  );
+  )
 }
+
+export default Home
